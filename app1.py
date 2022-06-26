68a8f83144332ae3c597b78bc4324279bb13a0b9
slimane ben miled <slimane.benmiled@fst.utm.tn>
2022-06-26 15:16:16 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
